3d03148fc5eb09ea5d257f6fe203b6fbde157461
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-24 10:02:19 -0300

    Create volunteer.php

---
+++ includes/sections/volunteer.php
@@ -0,0 +1,13 @@
+<section id="volunteer" class="section volunteer">
+    <div class="container">
+        <h2 class="section-title">Trabalho Voluntário</h2>
+        <div class="volunteer-content" style="margin-bottom: 32px; display: flex; flex-direction: column; align-items: flex-start;">
+            <h3 style="margin-bottom: 8px;">Grupo Escoteiro Cooper Cotia - 131</h3>
+            <p style="margin-left: 0;">Atualmente atuo como voluntário na Escoteira do Cooper Cotia do Distrito do Butantã SP, realizo trabalhos na equipe de apoio de Pais, atuando como suporte nas atividades, refeições e em Festas dos jovens do grupo Escoteiro aos finais de semana.</p>
+        </div>
+        <div class="volunteer-content" style="margin-bottom: 32px; display: flex; flex-direction: column; align-items: flex-start;">
+            <h3 style="margin-bottom: 8px;">Representante do GAP - CooperAtletico Clube</h3>
+            <p style="margin-left: 0;">Atuante no Grupo de apoio a prevenção realizando monitorias e inspeções do Clube CooperCotia em busca de melhorias e qualidade para os Socios.</p>
+        </div>
+    </div>
+</section>
