581561c35d15c2f0117d6196aab6e86889a2cd1d
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-24 09:59:50 -0300

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,22 @@
+<?php
+// Configurações básicas do site
+define('SITE_NAME', 'Alexandre Satochi Yamamoto - Especialista TI');
+define('SITE_DESCRIPTION', 'Profissional de TI e gestão com experiência em Desenvolvimento de Software e Infraestrutura');
+
+// URLs
+define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('ASSETS_URL', BASE_URL . 'assets/');
+
+// Links sociais
+$social_links = [
+    'github' => 'https://github.com/satochi-yamamoto',
+    'linkedin' => 'https://www.linkedin.com/in/alexyamamoto/',
+    'discord' => '#'
+];
+
+// Dados pessoais
+$personal_info = [
+    'email' => 'yamamoto@ydsoftware.com.br',
+    'linkedin' => 'linkedin.com/in/alexyamamoto'
+];
+?>
