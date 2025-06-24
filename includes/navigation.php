a2ec086746e632628572093c76e4acc0a9a14926
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-06-24 10:00:49 -0300

    Create navigation.php

---
+++ includes/navigation.php
@@ -0,0 +1,8 @@
+<ul>
+    <li><a href="#about">Sobre</a></li>
+    <li><a href="#experience">Experiência</a></li>
+    <li><a href="#education">Formação</a></li>
+    <li><a href="#skills">Habilidades</a></li>
+    <li><a href="#projects">Projetos</a></li>
+    <li><a href="#contact">Contato</a></li>
+</ul>
